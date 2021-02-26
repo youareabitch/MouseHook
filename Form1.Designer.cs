8489be73c4ad041e01ba8feea84f531dc40baf4e
Robert Lo <robert.lo@vertiv.com>
2021-02-26 15:32:27 +0800

    config feature and some fixs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace MouseHookTest
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,22 @@ namespace MouseHookTest
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hard Code Mode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -70,6 +82,7 @@ namespace MouseHookTest
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,9 @@ namespace MouseHookTest
         Thread yThread;
         int x = 0;
         int y = 0;
+        List<Config> configs = new List<Config>();
+        Config currentConfig;
+        Point cursorData = CorsorExtension.GetCursorPosition();
 
         public Form1()
         {
@@ -36,6 +39,20 @@ namespace MouseHookTest
             mouseHook.RightButtonUp += new MouseHookCallback(RightMouseUp);
             //Installing the Mouse Hooks
             mouseHook.Install();
+
+            //init configs
+            configs.Add(new Config());
+            configs.Add(new Config());
+            configs.Add(new Config());
+
+            //init current config
+            currentConfig = configs[0];
+
+            //hard code test
+            configs[0].Xconfig.Add(new ConfigDetail() { Offset = -1, Rate = 4 });
+            configs[0].Xconfig.Add(new ConfigDetail() { Offset = 1, Rate = 5 });
+            configs[0].Yconfig.Add(new ConfigDetail() { Offset = 2, Rate = 4 });
+            configs[0].Yconfig.Add(new ConfigDetail() { Offset = 0, Rate = 5 });
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -73,39 +90,59 @@ namespace MouseHookTest
         {
             xThread = new Thread(() =>
             {
+                bool switchFlag = false;
                 x = 0;
                 int offset = 1;
                 while (isLeftDown && isRightDown)
                 {
+                    cursorData = CorsorExtension.GetCursorPosition();
                     label1.InvokeIfRequired(() =>
                     {
-                        var cursorData = CorsorExtension.GetCursorPosition();
-
-                        //label1.Text = $"x:{x++}";
+                        cursorData = CorsorExtension.GetCursorPosition();
                         label1.Text = $"x:{cursorData.X} count:{x++}";
-                        Win32.SetCursorPos(cursorData.X + offset, cursorData.Y);
                     });
 
-                    offset = offset * -1;
-                    Thread.Sleep(20);
+                    if (button1.Text == "Hard Code Mode")
+                    {
+                        Win32.SetCursorPos(cursorData.X + offset, cursorData.Y);
+                        offset = offset * -1;
+                        Thread.Sleep(20);
+                    }
+                    else
+                    {
+                        cursorData = CorsorExtension.GetCursorPosition();
+                        Win32.SetCursorPos(cursorData.X + currentConfig.Xconfig[Convert.ToInt32(switchFlag)].Offset, cursorData.Y);
+                        Thread.Sleep(currentConfig.Xconfig[Convert.ToInt32(switchFlag)].Rate);
+                        switchFlag = !switchFlag;
+                    }
                 }
             });
             xThread.Start();
 
             yThread = new Thread(() =>
             {
+                var switchFlag = false;
                 y = 0;
                 while (isLeftDown && isRightDown)
                 {
                     label2.InvokeIfRequired(() =>
                     {
-                        var cursorData = CorsorExtension.GetCursorPosition();
-
-                        //label2.Text = $"y:{y++}";
-                        label2.Text = $"y:{cursorData.Y} count:{y++}";
-                        Win32.SetCursorPos(cursorData.X, cursorData.Y + 2);
+                        cursorData = CorsorExtension.GetCursorPosition();
+                        label2.Text = $"x:{cursorData.X} y:{cursorData.Y} count:{y++}";
                     });
-                    Thread.Sleep(20);
+
+                    if (button1.Text == "Hard Code Mode")
+                    {
+                        Win32.SetCursorPos(cursorData.X, cursorData.Y + 2);
+                        Thread.Sleep(20);
+                    }
+                    else
+                    {
+                        cursorData = CorsorExtension.GetCursorPosition();
+                        Win32.SetCursorPos(cursorData.X, cursorData.Y + currentConfig.Yconfig[Convert.ToInt32(switchFlag)].Offset);
+                        Thread.Sleep(currentConfig.Yconfig[Convert.ToInt32(switchFlag)].Rate);
+                        switchFlag = !switchFlag;
+                    }
                 }
             });
             yThread.Start();
@@ -124,6 +161,18 @@ namespace MouseHookTest
                 yThread.Abort();
             }
             yThread = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Hard Code Mode")
+            {
+                button1.Text = "Config Mode";
+            }
+            else
+            {
+                button1.Text = "Hard Code Mode";
+            }
         }
     }
 
